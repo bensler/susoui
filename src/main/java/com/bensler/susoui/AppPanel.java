8a1c539bee63af2d0315dae7e75ed5752403e5b8
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2023-01-28 13:06:00 +0100

    Constraint violation indication and suggestions added

--- src/main/java/com/bensler/susoui/AppPanel.java
+++ src/main/java/com/bensler/susoui/AppPanel.java
@@ -1,24 +1,11 @@
 package com.bensler.susoui;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
-import com.bensler.suso.Coordinate;
-import com.bensler.suso.FieldImpl;
 import com.bensler.suso.Game;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
@@ -31,103 +18,40 @@ public class AppPanel extends JPanel {
     SOLVE
   }
 
-  private final Map<JLabel, Coordinate> coordinatesByLabel;
-
   private final Game game;
+  private final GamePanel gamePanel;
 
   AppPanel() {
     super(new FormLayout(
       "5dlu:g, f:p, 15dlu, f:p, 5dlu:g", // columns
-      "5dlu:g, f:p, 5dlu:g"             // row
+      "5dlu:g, f:p, 5dlu:g"              // row
     ));
     final CellConstraints cc = new CellConstraints();
 
-    coordinatesByLabel = new HashMap<>();
-    game = new Game(SampleGames.SAMPLE_1);
-    add(createGamePanel(),    cc.rc(2, 2));
-    add(createControlPanel(), cc.rc(2, 4));
-  }
-
-  private JPanel createGamePanel() {
-    final LabelCreator labelCreator = new LabelCreator();
-    final JPanel gamePanel = new JPanel(new FormLayout(
-      labelCreator.rowColSpecs, // columns
-      labelCreator.rowColSpecs  // rows
-    ));
-    final CellConstraints cc = new CellConstraints();
-    final FieldImpl field = game.getField();
-
-    game.getCoordinates().forEach(coordinate -> {
-      final JLabel label = labelCreator.createLabel(
-        field.getDigit(coordinate).map(digit -> String.valueOf(digit.getNumber())).orElse(""),
-        true
-      );
-
-      coordinatesByLabel.put(label, coordinate);
-      gamePanel.add(label, cc.rc((coordinate.getY() * 2) + 1, (coordinate.getX() * 2) + 1));
-    });
-    return gamePanel;
+    game = new Game(SampleGames.SAMPLE_INVALID);
+    add(gamePanel = new GamePanel(game),  cc.rc(2, 2));
+    add(            createControlPanel(), cc.rc(2, 4));
   }
 
   private JPanel createControlPanel() {
     final JPanel controlPanel = new JPanel(new FormLayout(
-      "f:p",            // column
-      "f:p, 5dlu, f:p"  // rows
+      "c:p",                // column
+      "p, 5dlu, p, 5dlu:g"  // rows
     ));
     final CellConstraints cc = new CellConstraints();
-    final JButton button = new JButton("Button");
+    final JButton plusButton = new JButton("+");
+    final JButton minusButton = new JButton("-");
 
-    controlPanel.add(button, cc.rc(1, 1));
-    controlPanel.add(new GamePanel(), cc.rc(3, 1));
-    controlPanel.setBackground(Color.BLUE);
+    plusButton.addActionListener(event -> gamePanel.incCharSize());
+    minusButton.addActionListener(event -> gamePanel.decCharSize());
+    controlPanel.add(plusButton, cc.rc(1, 1));
+    controlPanel.add(minusButton, cc.rc(3, 1));
+    controlPanel.setBackground(new Color(200, 200, 255));
     return controlPanel;
   }
 
   protected void rightMouseClick(JLabel label) {
-    System.out.println(coordinatesByLabel.get(label));
-  }
-
-  class LabelCreator {
-
-    private final Dimension labelPrefSize;
-    private final Border labelBorder;
-    private final Font boldLabelFont;
-    private final String rowColSpecs;
-
-    LabelCreator() {
-      final JLabel label = new JLabel("m");
-      final Dimension prefSize;
-      final int maxDimension;
-      final String cellGroup;
-
-      boldLabelFont = label.getFont().deriveFont(Font.BOLD);
-      labelBorder = new CompoundBorder(new LineBorder(Color.BLACK), new EmptyBorder(5, 5, 5, 5));
-      label.setBorder(labelBorder);
-      prefSize = label.getPreferredSize();
-      maxDimension = Math.max(prefSize.width, prefSize.height);
-      labelPrefSize = new Dimension(maxDimension, maxDimension);
-      cellGroup = "f:p, 5dlu, f:p, 5dlu, f:p";
-      rowColSpecs = cellGroup + ", 8dlu, " + cellGroup + ", 8dlu, " + cellGroup;
-    }
-
-    JLabel createLabel(String text, boolean bold) {
-      final JLabel label = new JLabel(text);
-
-      label.setHorizontalAlignment(SwingConstants.CENTER);
-      if (bold) {
-        label.setFont(boldLabelFont);
-      }
-      label.setBorder(labelBorder);
-      label.setPreferredSize(labelPrefSize);
-      label.addMouseListener(new MouseAdapter() {
-        @Override
-        public void mousePressed(MouseEvent evt) {
-          AppPanel.this.rightMouseClick(label);
-        }
-      });
-      return label;
-    }
-
+    // TODO System.out.println(coordinatesByLabel.get(label));
   }
 
 }
--- src/main/java/com/bensler/susoui/GamePanel.java
+++ src/main/java/com/bensler/susoui/GamePanel.java
@@ -1,23 +1,214 @@
 package com.bensler.susoui;
 
+import static java.lang.Math.round;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.font.TextLayout;
-import java.awt.geom.Rectangle2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
 
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+import com.bensler.suso.Constraint;
+import com.bensler.suso.Coordinate;
+import com.bensler.suso.Digit;
+import com.bensler.suso.Game;
+import com.bensler.suso.Solver;
 
 public class GamePanel extends JComponent {
 
-  private static final Color DECKWEISS = new Color(255, 255, 255, 255);
-  private static final Color AQUARELL_ROT = new Color(255, 0, 0, 85);
+  class VariableSizes {
+
+    final int fontSize;
+    final Font plainFont;
+    final Font boldFont;
+    final int charSize;
+    final float cellSize;
+    final float halfCellSize;
+    final List<Float> rowColCenters;
+    final int widthHeight;
+
+    VariableSizes(int pFontSize) {
+      final BiFunction<List<Float>, Float, Float> addToLastElement = (
+        (list, toAdd) -> list.get(list.size() - 1) + toAdd
+      );
+      final Dimension text8Size;
+
+      fontSize = pFontSize;
+      plainFont = baseFont.deriveFont((float)fontSize);
+      boldFont = plainFont.deriveFont(Font.BOLD);
+      text8Size = getTextSize("8", boldFont);
+      charSize = Math.max(text8Size.width, text8Size.height);
+      cellSize = charSize * 2.5f;
+      halfCellSize = cellSize / 2.0f;
+
+      final float gap = charSize;
+      final float gapBlock = charSize * 2.0f;
+      final List<Float> block = new ArrayList<>();
+
+      block.add(halfCellSize);
+      block.add(block.get(0) + gap + cellSize);
+      block.add(block.get(1) + gap + cellSize);
+
+      final List<Float> result = new ArrayList<>();
+      final float start0 = gap;
+      block.stream().forEach(value -> result.add(start0 + value));
+      final float start1 = addToLastElement.apply(result, halfCellSize + gapBlock);
+      block.stream().forEach(value -> result.add(start1 + value));
+      final float start2 = addToLastElement.apply(result, halfCellSize + gapBlock);
+      block.stream().forEach(value -> result.add(start2 + value));
+      widthHeight = round(addToLastElement.apply(result, gap + halfCellSize));
+      rowColCenters = List.copyOf(result);
+    }
+
+    OptionalInt getRowCol(double position) {
+      return IntStream.range(0, rowColCenters.size()).filter(index -> {
+        final float cellCenter = rowColCenters.get(index);
+
+        return (position > (cellCenter - halfCellSize)) && (position < (cellCenter + halfCellSize));
+      }).findFirst();
+    }
+
+  }
+
+  private static final float CONSTRAINT_EXTEND = 1.3f;
+  private static final Color BACKGROUND_WHITE = Color.WHITE;
+  private static final Color CONSTRAINT_VIOLATED_RED = new Color(255, 0, 0, 85);
+  private static final Color CHARACTER_BLACK = Color.BLACK;
+  private static final Color CHARACTER_DARK_RED = new Color(255, 0, 0);
+  private static final Color FRAME_BLACK = Color.BLACK;
+  private static final Color SELECTION_INDICATOR_GREEN = Color.GREEN;
   private static final Color DECKROT = new Color(255, 0, 0);
 
-  public GamePanel() {
-    setPreferredSize(new Dimension(70, 70));
+  private final Font baseFont;
+  private final Game game;
+  private final Solver solver;
+
+  private Optional<Coordinate> selectedCell;
+  private VariableSizes sizes;
+
+  public GamePanel(Game pGame) {
+    baseFont = new JLabel().getFont();
+    solver = new Solver(game = pGame);
+    setFontSize(15);
+    selectedCell = Optional.empty();
+    addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent evt) {
+        GamePanel.this.mouseClicked(evt);
+      }
+    });
+  }
+
+  private void mouseClicked(MouseEvent evt) {
+    final Point position = evt.getPoint();
+    final OptionalInt row = sizes.getRowCol(position.getY());
+    final OptionalInt col = sizes.getRowCol(position.getX());
+
+    selectedCell = Optional.ofNullable(
+      (row.isPresent() && col.isPresent())
+      ? new Coordinate(col.getAsInt(), row.getAsInt())
+      : null
+    );
+    requestFocusInWindow();
+    repaint();
+  }
+
+  @Override
+  public Dimension getPreferredSize() {
+    return new Dimension(sizes.widthHeight, sizes.widthHeight);
+  }
+
+  private void setFontSize(int pFontSize) {
+    sizes = new VariableSizes(pFontSize);
+    revalidate();
+    repaint();
+  }
+
+  private void drawCell(Graphics2D g, Coordinate coordinate, Map<Constraint, Set<Digit>> failingConstraints) {
+    final Point2D center = new Point2D.Float(
+      sizes.rowColCenters.get(coordinate.getX()),
+      sizes.rowColCenters.get(coordinate.getY())
+    );
+    final float halfCellSize = sizes.cellSize / 2.0f;
+    final boolean suggestMove = true;
+    final int intCellSize = round(sizes.cellSize);
+    final Font font = sizes.boldFont;
+    final Optional<Digit> optionalDigit = game.getDigit(coordinate);
+
+    g.setColor(FRAME_BLACK);
+    g.drawRect(
+      (int)round(center.getX() - halfCellSize),
+      (int)round(center.getY() - halfCellSize),
+      intCellSize,
+      intCellSize
+    );
+
+    selectedCell.filter(coordinate::equals).ifPresent(selectedCoordinate -> {
+      final float thirdCellSize = sizes.cellSize / 3.0f;
+      final int twoThirdCellSize = round(thirdCellSize * 2.0f);
+
+      g.setColor(Color.GREEN);
+      g.drawRect(
+        (int)round(center.getX() - thirdCellSize),
+        (int)round(center.getY() - thirdCellSize),
+        twoThirdCellSize,
+        twoThirdCellSize
+      );
+    });
+    optionalDigit.map(digit -> String.valueOf(digit.getNumber())).ifPresentOrElse(str -> {
+      final Dimension textSize = getTextSize(str, font);
+      final boolean troubleMaker = failingConstraints.entrySet().stream().filter(
+        entry -> entry.getKey().covers(coordinate)
+      ).map(entry -> entry.getValue())
+      .flatMap(digits -> digits.stream())
+      .anyMatch(duplicateDigit -> duplicateDigit.equals(optionalDigit.get()));
+
+      g.setColor(troubleMaker ? CHARACTER_DARK_RED : CHARACTER_BLACK);
+      g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+      g.setFont(font);
+      g.drawString(
+        str,
+        round(center.getX() - (textSize.width  / 2.0f) + 1),
+        round(center.getY() - (textSize.height / 2.0f) + 1) + textSize.height
+      );
+    }, () -> {
+      if (suggestMove && (solver.findPossibleDigits(coordinate).size() == 1)) {
+        g.setColor(SELECTION_INDICATOR_GREEN);
+        g.fillRect(
+          (int)round(center.getX() - halfCellSize) + 1,
+          (int)round(center.getY() - halfCellSize) + 1,
+          intCellSize - 1,
+          intCellSize - 1
+        );
+      }
+    });
+  }
+
+  private Dimension getTextSize(String str, Font pFont) {
+    final FontMetrics fontMetrics = getFontMetrics(pFont);
+
+    return new Dimension(
+      fontMetrics.stringWidth(str),
+      fontMetrics.getAscent() - (fontMetrics.getDescent() / 2)
+    );
   }
 
   @Override
@@ -25,19 +216,34 @@ public class GamePanel extends JComponent {
     super.paintComponent(g);
     final Graphics2D graphics = (Graphics2D) g;
     final Dimension size = getSize();
-    Font font = getFont().deriveFont(Font.BOLD);
-    Rectangle2D stringBounds = new TextLayout("8", font, graphics.getFontRenderContext()).getBounds();
-    graphics.setColor(DECKWEISS);
+    final Map<Constraint, Set<Digit>> failingConstraints = game.getFailingConstraints();
+
+    graphics.setColor(BACKGROUND_WHITE);
     graphics.fillRect(0, 0, size.width, size.height);
-    graphics.setColor(AQUARELL_ROT);
-    graphics.fillRect(10, 10, 30, 30);
-    graphics.fillRect(20, 20, 30, 30);
-    graphics.fillRect(30, 30, 30, 30);
-    graphics.setColor(DECKROT);
-    graphics.fillRect(45, 10, 15, 15);
-    graphics.setFont(font);
-    graphics.drawString("8", 10, 50);
+    failingConstraints.keySet().forEach(constraint -> {
+      final Rectangle bounds = constraint.getBounds();
+      final float x = sizes.rowColCenters.get(bounds.x);
+      final float y = sizes.rowColCenters.get(bounds.y);
+      final float w = sizes.rowColCenters.get(bounds.x + bounds.width) - x;
+      final float h = sizes.rowColCenters.get(bounds.y + bounds.height) - y;
+
+      g.setColor(CONSTRAINT_VIOLATED_RED);
+      g.fillRect(
+        round(x - ((sizes.cellSize / 2) * CONSTRAINT_EXTEND)),
+        round(y - ((sizes.cellSize / 2) * CONSTRAINT_EXTEND)),
+        round(w + (sizes.cellSize * CONSTRAINT_EXTEND)),
+        round(h + (sizes.cellSize * CONSTRAINT_EXTEND))
+      );
+    });
+    game.getCoordinates().forEach(coordinate -> drawCell(graphics, coordinate, failingConstraints));
   }
 
+  void incCharSize() {
+    setFontSize(sizes.fontSize + 1);
+  }
+
+  void decCharSize() {
+    setFontSize(sizes.fontSize - 1);
+  }
 
 }
