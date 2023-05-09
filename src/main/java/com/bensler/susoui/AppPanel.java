784e87e2394b83b6ec7c200d59725980a74a707f
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2023-05-09 07:51:14 +0200

    BooleanCriteriaPanel introduced

--- src/main/java/com/bensler/susoui/AppPanel.java
+++ src/main/java/com/bensler/susoui/AppPanel.java
@@ -1,9 +1,12 @@
 package com.bensler.susoui;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
@@ -40,15 +43,17 @@ public class AppPanel extends JPanel {
 
   private JPanel createControlPanel() {
     final JPanel controlPanel = new JPanel(new FormLayout(
-      "f:p",              // column
-      "f:p, 5dlu:g, f:p"  // rows
+      "f:p",          // column
+      "p, 5dlu, p, 5dlu, p, 5dlu:g, p"  // rows
     ));
     final CellConstraints cc = new CellConstraints();
     final JCheckBox cbSuggest = new JCheckBox("Suggestions");
 
     cbSuggest.addActionListener(event -> gamePanel.setSuggestions(cbSuggest.isSelected()));
     controlPanel.add(cbSuggest, cc.rc(1, 1));
-    controlPanel.add(createViewSizePanel(), cc.rc(3, 1));
+    controlPanel.add(new BooleanCriteriaPanel("determined"), cc.rc(3, 1));
+    controlPanel.add(new BooleanCriteriaPanel("solvable"), cc.rc(5, 1));
+    controlPanel.add(createViewSizePanel(), cc.rc(7, 1));
     controlPanel.setBackground(new Color(200, 200, 255));
     return controlPanel;
   }
@@ -75,6 +80,29 @@ public class AppPanel extends JPanel {
     viewSizePanel.add(plusButton, cc.rc(1, 1));
     viewSizePanel.add(minusButton, cc.rc(1, 3));
     return viewSizePanel;
+  }
+
+  static class BooleanCriteriaPanel extends JPanel implements ActionListener {
+
+    private final JCheckBox checkbox;
+    private final JLabel indicator;
+
+    BooleanCriteriaPanel(String text) {
+      super(new FormLayout("p, 5dlu:g, p", "p"));
+
+      final CellConstraints cc = new CellConstraints();
+
+      add(checkbox = new JCheckBox(text), cc.rc(1, 1));
+      add(indicator = new JLabel(), cc.rc(1, 3));
+      checkbox.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      // TODO Auto-generated method stub
+
+    }
+
   }
 
 }
